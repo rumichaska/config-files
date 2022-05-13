aa84a9eb0cbd97aeac8c887d0410c2a42a9f7261
Diego Castro G <castrogda@gmail.com>
2022-05-12 20:01:29 -0500

    Se añadió pyright al lsp. NOTE: instalar pyright de forma local.

--- .config/nvim/lua/user/lsp/init.lua
+++ .config/nvim/lua/user/lsp/init.lua
@@ -39,6 +39,12 @@ lsp_installer.on_server_ready(function(server)
         opts = vim.tbl_deep_extend("force", r_lang_opts, opts)
     end
 
+    -- LSP Python: pyright
+    if server.name == "pyright" then
+        local pyright_opts = require("user.lsp.settings.pyright_lua")
+        opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+    end
+
     -- This setup() function is exactly the same as lspconfig's setup function.
     -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
     server:setup(opts)
