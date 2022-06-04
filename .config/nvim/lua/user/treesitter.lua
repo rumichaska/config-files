850712ec72b7c37370377dc7ebca7ace08e7e29f
Diego Castro G <castrogda@gmail.com>
2022-06-03 20:37:54 -0500

    Actualización de archivo treesitter. NOTE: revisión con documentación mas reciente.

--- .config/nvim/lua/user/treesitter.lua
+++ .config/nvim/lua/user/treesitter.lua
@@ -3,31 +3,34 @@ local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
 	return
 end
+--
+-- Parsers con soporte
+local ts_parser = {
+    "css",
+    "lua",
+    "markdown",
+    "python",
+    "r",
+}
 
 -- Configuración de treesitter
 configs.setup({
     -- Configuración de treesitter, modificar según lenguajes
-    -- ensure_installed = "maintained",    -- Solo instalar lenguajes con soporte
-    ensure_installed = "all",    -- Solo instalar lenguajes con soporte
-    sync_install = false,               -- Sincronización solo lenguajes con soporte
-    ignore_install = {},                -- Lista de `parsers` a ignorar en instalación
+    ensure_installed = ts_parser,   -- Solo instalar lenguajes con soporte
+    sync_install = false,           -- Sincronización solo lenguajes con soporte
+    ignore_install = {},            -- Lista de `parsers` a ignorar en instalación
     highlight = {
-        enable = true,                  -- `false` deshabilitará la extensión
-        disable = {},
+        enable = true,              -- `false` deshabilitará la extensión
         additional_vim_regex_highlighting = false,
     },
     indent = {
         enable = true,
-        disable = { "yaml" }
     },
     -- Agregando configuración complementaria de nvim-ts-rainbow
     rainbow = {
         enable = true,
-        -- disable = { "jsx", "cpp" },  -- Listado de lenguajes a omitir
         extended_mode = true,
         max_file_lines = nil,
-        -- colors = {},                 -- Tabla con colores HEX
-        -- termcolors = {},             -- Tabla con nombre de colores
     },
     -- Agregando configuración para comentarios según lenguajes
     context_commentstring = {
