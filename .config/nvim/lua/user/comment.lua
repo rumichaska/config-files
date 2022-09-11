1cef4fa9a6e097c3cd72a79182fea3e4c406aa11
Diego Castro G <castrogda@gmail.com>
2022-09-11 18:19:03 -0500

    Se corrigió error de comentarios en bloques y líneas.

--- .config/nvim/lua/user/comment.lua
+++ .config/nvim/lua/user/comment.lua
@@ -9,9 +9,9 @@ comment.setup({
     -- Función para comentarios según lenguaje
     pre_hook = function(ctx)
         local U = require("Comment.utils")
-        local key = ctx.ctype == U.ctype.line and "__default" or "__multiline"
+        local key = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
         local location = nil
-        if ctx.ctype == U.ctype.block then
+        if ctx.ctype == U.ctype.blockwise then
             location = require("ts_context_commentstring.utils").get_cursor_location()
         elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
             location = require("ts_context_commentstring.utils").get_visual_start_location()
