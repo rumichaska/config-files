346aa7412c6842f9152caed4dddfc94ed26ca062
Diego Castro G <castrogda@gmail.com>
2022-09-17 21:14:48 -0500

    Se agregó mapeo para ejecutar código en terminal.

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -11,3 +11,6 @@ local keymap = vim.keymap.set
 
 -- Lanzar consola de PYTHON
 keymap("n", "<Leader>tp", ":lua _PYTHON_TOGGLE()<CR>", opts)
+
+-- Ejecutar código en terminal
+keymap("n", "<Leader>L", ":1TermExec cmd='python3 %:p:.'<CR>")
