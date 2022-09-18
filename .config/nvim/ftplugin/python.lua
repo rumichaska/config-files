91fdf0edecdd99bab944a1932179879ec087f89b
Diego Castro G <castrogda@gmail.com>
2022-09-17 21:42:50 -0500

    Agregando mapeos para comentarios.

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -12,5 +12,13 @@ local keymap = vim.keymap.set
 -- Lanzar consola de PYTHON
 keymap("n", "<Leader>tp", ":lua _PYTHON_TOGGLE()<CR>", opts)
 
+-- Enviar código a consola de python3
+keymap("n", "<Leader>l", ":ToggleTermSendCurrentLine 20<CR>j0")
+keymap("v", "<Leader>l", ":ToggleTermSendVisualLines 20<CR>")
+keymap("v", "<Leader>l", ":ToggleTermSendVisualSelection 20<CR>")
+
 -- Ejecutar código en terminal
 keymap("n", "<Leader>L", ":1TermExec cmd='python3 %:p:.'<CR>")
+
+-- Comentario de sección
+keymap("n", "gch", "73i-<Esc>0:normal gcc<CR>2l<S-R>", opts)
