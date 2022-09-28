783f01b86d87e2eaa5e6625fc7e85a1464e3cff3
Diego Castro G <castrogda@gmail.com>
2022-09-27 22:54:24 -0500

    Se activó sombrado de números y 'blame' de línea.

--- .config/nvim/lua/user/gitsigns.lua
+++ .config/nvim/lua/user/gitsigns.lua
@@ -14,7 +14,7 @@ gitsigns.setup({
         changedelete = { hl = "GitSignsChange", text = "▌", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
     },
     signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-    numhl = false,      -- Toggle with `:Gitsigns toggle_numhl`
+    numhl = true,      -- Toggle with `:Gitsigns toggle_numhl`
     linehl = false,     -- Toggle with `:Gitsigns toggle_linehl`
     word_diff = false,  -- Toggle with `:Gitsigns toggle_word_diff`
     watch_gitdir = {
@@ -22,10 +22,10 @@ gitsigns.setup({
         follow_files = true,
     },
     attach_to_untracked = true,
-    current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts = {
         virt_text = true,
-        virt_text_pos = "overlay",  -- 'eol' | 'overlay' | 'right_align'
+        virt_text_pos = "eol",  -- 'eol' | 'overlay' | 'right_align'
         delay = 1000,
         ignore_whitespace = false,
     },
