210f7719559bff3ccf16715a8b8857978ba4111e
Diego Castro G <castrogda@gmail.com>
2022-10-01 23:08:37 -0500

    Se actualizó configuración. TODO: Mejorar personalización.

--- .config/nvim/lua/user/indentline.lua
+++ .config/nvim/lua/user/indentline.lua
@@ -5,46 +5,47 @@ if not status_ok then
 end
 
 -- Configuración de indent_blankline
-vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-vim.g.indent_blankline_filetype_exclude = {
-    "help",
-    "packer",
-    "NvimTree",
-    "Trouble",
-    "dashboard",
-    "neogitstatus",
-    "startify",
-}
-vim.g.indentLine_enabled = 1
-vim.g.indent_blankline_char = "│"
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_show_first_indent_level = true
-vim.g.indent_blankline_use_treesitter = true
-vim.g.indent_blankline_show_current_context = true
-vim.g.indent_blankline_context_patterns = {
-    "class",
-    "return",
-    "function",
-    "method",
-    "^if",
-    "^while",
-    "jsx_element",
-    "^for",
-    "^object",
-    "^table",
-    "block",
-    "arguments",
-    "if_statement",
-    "else_clause",
-    "jsx_element",
-    "jsx_self_closing_element",
-    "try_statement",
-    "catch_clause",
-    "import_statement",
-    "operation_type",
-}
-
 indent_blankline.setup({
-    show_end_of_line = true,
-    space_char_blankline = " "
+    -- char_blankline = "┆",
+    use_treesitter = true,
+    -- max_indent_increase = 1,
+    -- show_first_indent_level = true,
+    -- show_trailing_blankline_indent = true,
+    -- show_end_of_line = true,
+    -- filetype_exclude = {
+    --     "lspinfo",
+    --     "packer",
+    --     "checkhealth",
+    --     "help",
+    --     "man",
+    --     "",
+    -- },
+    -- buftype_exclude = {
+    --     "terminal",
+    --     "nofile",
+    --     "quickfix",
+    --     "prompt",
+    -- },
+    show_current_context = true,
+    -- context_char_blankline = "┆",
+    -- context_patterns = {
+    --     "class",
+    --     "^func",
+    --     "method",
+    --     "^if",
+    --     "while",
+    --     "for",
+    --     "with",
+    --     "try",
+    --     "except",
+    --     "arguments",
+    --     "argument_list",
+    --     "object",
+    --     "dictionary",
+    --     "element",
+    --     "table",
+    --     "tuple",
+    --     "do_block",
+    -- },
+    use_treesitter_scope = true,
 })
