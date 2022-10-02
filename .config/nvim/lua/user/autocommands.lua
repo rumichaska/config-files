8911d356a3f5048b2fc27470edb9045de541d1bf
Diego Castro G <castrogda@gmail.com>
2022-10-01 23:54:43 -0500

    Se agregó autocommand para cerrar ventanas de apoyo.

--- .config/nvim/lua/user/autocommands.lua
+++ .config/nvim/lua/user/autocommands.lua
@@ -1,4 +1,19 @@
--- Autogroups personalizados
--- NOTE: no dependen de 'filetype' o de hacerlo es muy complejo
+-- Autocommands personalizados
 
 -- Globales
+
+-- Cerrar ventanas de apoyo (help, lsp, etc.)
+vim.api.nvim_create_autocmd({ "Filetype" }, {
+    pattern = {
+        "qf",
+        "help",
+        "man",
+        "lspinfo",
+    },
+    callback = function()
+        local opts = { noremap = true, silent = true, buffer = 0 }
+        local keymap = vim.keymap.set
+        keymap("n", "q", ":clo<CR>", opts)
+    end,
+    group = vim.api.nvim_create_augroup("CloseInfoBuffer", { clear = true })
+})
