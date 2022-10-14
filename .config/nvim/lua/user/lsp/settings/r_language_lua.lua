54f29f2c7328c5fd201fca06b13281872acd9c20
Diego Castro G <castrogda@gmail.com>
2022-10-14 14:34:54 -0500

    Se mejoró detección de root directory.

--- .config/nvim/lua/user/lsp/settings/r_language_lua.lua
+++ .config/nvim/lua/user/lsp/settings/r_language_lua.lua
@@ -1,8 +1,16 @@
 -- Configuración de LSP R language
+
+-- Control de mensaje de errores por falta de lspconfig
+local lspconfig_status_ok, util = pcall(require, "lspconfig.util")
+if not lspconfig_status_ok then
+    return
+end
+
+-- Configuración
 return {
-    root_dir = function()
+    root_dir = function(fname)
         -- Patrón de búsqueda de directorio de trabajo
-        return vim.fs.dirname(vim.fs.find({ ".git", "*.Rproj" }, { upward = true })[1])
+        return util.root_pattern("*.Rproj")(fname) or util.find_git_ancestor(fname) or vim.loop.os_homedir()
     end,
     settings = {
         r = {
