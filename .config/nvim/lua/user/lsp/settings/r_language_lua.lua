b45260507c697013b3374a98c66fee76ba1cba2c
Diego Castro G <castrogda@gmail.com>
2022-10-14 11:09:01 -0500

    Mejora de detección de directorio de trabajo.

--- .config/nvim/lua/user/lsp/settings/r_language_lua.lua
+++ .config/nvim/lua/user/lsp/settings/r_language_lua.lua
@@ -1,5 +1,9 @@
 -- Configuración de LSP R language
 return {
+    root_dir = function()
+        -- Patrón de búsqueda de directorio de trabajo
+        return vim.fs.dirname(vim.fs.find({ ".git", "*.Rproj" }, { upward = true })[1])
+    end,
     settings = {
         r = {
             lsp = {
