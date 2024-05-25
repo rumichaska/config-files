737a5de7b7ae330cfa3572f228b5e73fd101ee08
Diego Castro G <castrogda@gmail.com>
2024-05-24 21:31:11 -0500

    neovim: Limpieza de configuración y actualización de plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,46 +6,25 @@ if not vim.loop.fs_stat(lazypath) then
         "clone",
         "--filter=blob:none",
         "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
+        "--branch=stable",
         lazypath,
     })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Add lazy.nvim and options
--- NOTE: plugins.core load config.options with init function
+-- Add lazy.nvim config
 require("lazy").setup("plugins", {
     defaults = { lazy = true, version = false },
-    install = { colorscheme = { "tokyonight", "catppuccin", "habamax" } },
+    install = { colorscheme = { "catppuccin" } },
     ui = {
         size = { width = 0.8, height = 0.9 },
         border = "rounded",
         icons = {
             cmd = " ",
-            config = " ",
-            event = "",
-            ft = " ",
-            init = " ",
-            import = " ",
-            keys = " ",
-            lazy = "󰒲 ",
-            loaded = "●",
-            not_loaded = "○",
-            plugin = " ",
-            runtime = " ",
-            require = "󰢱 ",
-            source = " ",
-            start = "󰼛",
-            task = "✔ ",
-            list = {
-                "●",
-                "➜",
-                "★",
-                "‒",
-            },
+            start = "󰼛 ",
         },
     },
-    checker = { enabled = true },
+    checker = { enabled = true, notify = false },
     performance = {
         rtp = {
             disabled_plugins = {
