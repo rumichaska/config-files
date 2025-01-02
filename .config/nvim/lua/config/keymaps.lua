9f684ff10fe108c3ba2248a62ddcdd2317d0ed8a
Diego Castro G <castrogda@gmail.com>
2025-01-02 16:28:00 -0500

    neovim: Actualización de keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,5 @@
 -- Highlight on search
-vim.keymap.set("n", "<Esc>", "<Cmd>nohlsearch<CR>", { desc = "Clear highlights" })
+vim.keymap.set({ "n", "i" }, "<Esc>", "<Cmd>nohlsearch<CR><Esc>", { desc = "Clear highlights" })
 
 -- Make split navigation easier.
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
@@ -7,12 +7,18 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Buffer navigation
+vim.keymap.set("n", "<Leader>bb", "<Cmd>e #<Cr>", { desc = "Switch to last buffer" })
+
 -- Resize window using <ctrl> arrow keys
 vim.keymap.set("n", "<C-Up>", "<Cmd>resize +2<CR>", { desc = "Increase window height" })
 vim.keymap.set("n", "<C-Down>", "<Cmd>resize -2<CR>", { desc = "Decrease window height" })
 vim.keymap.set("n", "<C-Left>", "<Cmd>vertical resize -2<CR>", { desc = "Decrease window width" })
 vim.keymap.set("n", "<C-Right>", "<Cmd>vertical resize +2<CR>", { desc = "Increase window width" })
-vim.keymap.set("n", "<Leader>ur", "<Cmd>wincmd=<CR>", { desc = "Restore window size" })
+vim.keymap.set(
+  "n", "<Leader>ur", "<cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
+  { desc = "Redraw / clear hlsearch / diff update" }
+)
 
 -- Terminal
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
@@ -22,21 +28,25 @@ vim.keymap.set("t", "<C-j>", "<Cmd>wincmd j<CR>", { desc = "Move focus to lower 
 vim.keymap.set("t", "<C-k>", "<Cmd>wincmd k<CR>", { desc = "Move focus to upper window" })
 
 -- Windows
-vim.keymap.set("n", "<Leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
-vim.keymap.set("n", "<Leader>w-", "<C-W>s", { desc = "Split Window Below", remap = true })
-vim.keymap.set("n", "<Leader>w|", "<C-W>v", { desc = "Split Window Right", remap = true })
+vim.keymap.set("n", "<Leader>wd", "<C-W>c", { desc = "Delete window", remap = true })
+vim.keymap.set("n", "<Leader>w-", "<C-W>s", { desc = "Split window below", remap = true })
+vim.keymap.set("n", "<Leader>w|", "<C-W>v", { desc = "Split window right", remap = true })
 
 -- Better up/down
 vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
 vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
 
 -- Move Lines using <alt> jk keys
-vim.keymap.set("n", "<A-j>", "<Cmd>m .+1<CR>==", { desc = "Move Down" })
-vim.keymap.set("n", "<A-k>", "<Cmd>m .-2<CR>==", { desc = "Move Up" })
-vim.keymap.set("i", "<A-j>", "<Esc><Cmd>m .+1<CR>==gi", { desc = "Move Down" })
-vim.keymap.set("i", "<A-k>", "<Esc><Cmd>m .-2<CR>==gi", { desc = "Move Up" })
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move Down" })
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move Up" })
+vim.keymap.set("n", "<A-j>", "<Cmd>m .+1<CR>==", { desc = "Move down" })
+vim.keymap.set("n", "<A-k>", "<Cmd>m .-2<CR>==", { desc = "Move up" })
+vim.keymap.set("i", "<A-j>", "<Esc><Cmd>m .+1<CR>==gi", { desc = "Move down" })
+vim.keymap.set("i", "<A-k>", "<Esc><Cmd>m .-2<CR>==gi", { desc = "Move up" })
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move up" })
+
+-- n always forward and N always backwards in search with / and ?
+vim.keymap.set({ "n", "x", "o" }, "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+vim.keymap.set({ "n", "x", "o" }, "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
 
 -- Lazy
 vim.keymap.set("n", "<Leader>l", "<Cmd>Lazy<CR>", { desc = "Lazy" })
