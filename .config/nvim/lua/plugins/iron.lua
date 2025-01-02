31f7296d040e46cf15fd99568a0a5dc602d3e7e6
Diego Castro G <castrogda@gmail.com>
2025-01-02 16:29:23 -0500

    neovim: Actualización de opciones de REPL

--- .config/nvim/lua/plugins/iron.lua
+++ .config/nvim/lua/plugins/iron.lua
@@ -4,32 +4,33 @@ return {
   main = "iron.core",
   opts = function()
     -- Remove extra <CR> when sending lines to R console
-    -- local bracketed_paste_radian = function(lines)
-    --   local open_code = "\27[200~"
-    --   local close_code = "\27[201~"
-    --   local cr = "\13"
-    --   if #lines == 1 then
-    --     return { lines[1] .. cr }
-    --   else
-    --     local new = { open_code .. lines[1] }
-    --     for line = 2, #lines do
-    --       table.insert(new, lines[line])
-    --     end
-    --
-    --     table.insert(new, close_code)
-    --     return new
-    --   end
-    -- end
+    local bracketed_paste_radian = function(lines)
+      local open_code = "\27[200~"
+      local close_code = "\27[201~"
+      local cr = "\13"
+      if #lines == 1 then
+        return { lines[1] .. cr }
+      else
+        local new = { open_code .. lines[1] }
+        for line = 2, #lines do
+          table.insert(new, lines[line])
+        end
+
+        table.insert(new, close_code)
+        return new
+      end
+    end
     return {
       config = {
+        highlight_last = false,
         scratch_repl = true,
         close_window_on_exit = true,
         repl_definition = {
-          -- r = {
-          --   command = { "radian" },
-          --   format = bracketed_paste_radian,
-          -- },
-          r = require("iron.fts.r").radian,
+          r = {
+            command = { "radian" },
+            format = bracketed_paste_radian,
+          },
+          -- r = require("iron.fts.r").radian,
           python = require("iron.fts.python").ipython,
           julia = require("iron.fts.julia").julia,
         },
