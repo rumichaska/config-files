1e86f757372b0aae79327acc4b44f27a392a79f9
Diego Castro G <castrogda@gmail.com>
2025-02-26 10:27:12 -0500

    neovim: Implementación de treesitter para quarto y rmd

--- .config/nvim/after/ftplugin/rmd.lua
+++ .config/nvim/after/ftplugin/rmd.lua
@@ -4,14 +4,14 @@ vim.opt_local.wrap = true
 vim.opt_local.spell = false
 
 -- Set highlight as markdown
-vim.treesitter.language.register("markdown", { "quarto" })
+vim.treesitter.language.register("markdown", { "rmd" })
 
 -- Functions and variables
 local map = function(mode, lhs, rhs, desc)
   desc = desc or ""
   vim.keymap.set(mode, lhs, rhs, { buffer = 0, desc = "R: " .. desc })
 end
-local source = "quarto render " .. vim.fn.expand("%")
+-- local source = "quarto render " .. vim.fn.expand("%")
 local function augroup(name)
   return vim.api.nvim_create_augroup("quarto_" .. name, { clear = true })
 end
@@ -20,7 +20,7 @@ end
 map("i", "<C-S-m>", "<Esc>:normal! a |><CR>a ", "Base pipe operator")
 map("i", "<C-S-i>", "<Esc>:normal! a %in%<CR>a ", "`%in%` operator")
 map("i", "<M-->", "<Esc>:normal! a <-<CR>a ", "Asign operator")
-map("n", "<M-s>", "<Cmd>!" .. source .. "<CR>", "Render quarto file")
+-- map("n", "<M-s>", "<Cmd>!" .. source .. "<CR>", "Render quarto file")
 
 -- Autocommands
 vim.api.nvim_create_autocmd("TermOpen", {
