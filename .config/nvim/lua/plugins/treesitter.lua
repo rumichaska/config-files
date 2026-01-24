ac13232a345d8b39b17a0179879cc43354d13f8a
Diego Castro G <castrogda@gmail.com>
2026-01-24 12:05:47 -0500

    neovim: Activación de treesitter para bash y scss

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,34 +5,32 @@ return {
   build = ":TSUpdate",
   config = function()
     local ts = require("nvim-treesitter")
-    local parsers = { "bash", "css", "html", "json", "python", "r" }
+    local parsers = { "bash", "css", "html", "json", "python", "r", "scss" }
     local Util = require("util")
 
     -- Install parsers
     ts.install(parsers)
 
-    -- Enable treesitter features
-    local install_parser_and_enable_features = function(event)
-      local lang = event.match
-
-      -- Enable syntax highlighting for the buffer
-      local ok, _ = pcall(vim.treesitter.start, event.buf, lang)
-      if not ok then return end
-
-      -- Enable folds in window-local
-      vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-      vim.wo.foldmethod = "expr"
-      vim.wo.foldlevel = 99
-
-      -- Enable indentation in buffer-local
-      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-    end
-
     -- Run treesitter features
     vim.api.nvim_create_autocmd("Filetype", {
       group = Util.augroup("treesitter"),
       desc = "Enable treesitter and features",
-      callback = install_parser_and_enable_features
+      callback = function(args)
+        -- Language
+        local lang = args.match == "sh" and "bash" or args.match
+
+        -- Enable syntax highlighting for the buffer
+        local ok, _ = pcall(vim.treesitter.start, args.buf, lang)
+        if not ok then return end
+
+        -- Enable folds in window-local
+        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.wo.foldmethod = "expr"
+        vim.wo.foldlevel = 99
+
+        -- Enable indentation in buffer-local
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end,
 }
