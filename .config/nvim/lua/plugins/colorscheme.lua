24d72334818995619ae267586093100d66387e22
Diego Castro G <castrogda@gmail.com>
2026-01-28 14:33:19 -0500

    neovim: Retiro de configuración para kanagawa

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,35 +1,4 @@
 return {
-  -- Kanagawa
-  {
-    "rebelot/kanagawa.nvim",
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-    opts = {
-      colors = {
-        palette = { dragonBlack3 = "#000000" },
-        theme = {
-          all = {
-            ui = { bg_gutter = "none" },
-          },
-        },
-      },
-      overrides = function(colors)
-        local theme = colors.theme
-        return {
-          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
-          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-          PmenuSbar = { bg = theme.ui.bg_m1 },
-          PmenuThumb = { bg = theme.ui.bg_p2 },
-        }
-      end,
-      theme = "dragon",
-    },
-    config = function(_, opts)
-      require("kanagawa").setup(opts)
-      vim.cmd.colorscheme("kanagawa")
-    end,
-  },
   -- Catppuccin
   {
     "catppuccin/nvim",
