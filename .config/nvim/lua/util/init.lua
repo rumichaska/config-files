7d8bc2d236c2dec2c3ef297de85c71a379cc9a33
Diego Castro G. <castrogda@gmail.com>
2026-05-08 12:55:06 -0500

    neovim: Mejora de funciones de apoyo

--- .config/nvim/lua/util/init.lua
+++ .config/nvim/lua/util/init.lua
@@ -5,26 +5,77 @@ function M.augroup(name)
   return vim.api.nvim_create_augroup("Config_" .. name, { clear = true })
 end
 
--- Check if plugin is present and enabled
-function M.has(plugin)
-  return require("lazy.core.config").spec.plugins[plugin] ~= nil
-end
-
--- Get plugin opts as a table
-function M.opts(name)
-  local plugin = require("lazy.core.config").plugins[name]
-  if not plugin then
-    return {}
+-- Generate short path name
+-- Example:
+-- * /home/.config/nvim -> h/c/n
+-- * /media/killa/Trabajo_Externo/CDC -> m/k/t/c
+local function short_path_name(path)
+  local dirs = {}
+  for dir in path:gmatch("[^/]+") do
+    local letter = dir:gsub("^%.*", ""):sub(1, 1)
+    if letter ~= "" then
+      table.insert(dirs, letter:lower())
+    end
   end
-  local Plugin = require("lazy.core.plugin")
-  return Plugin.values(plugin, "opts", false)
+  return table.concat(dirs, "/")
 end
 
--- Get foreground color from highlight group
-function M.fg(name)
-  local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name })
-  local fg = hl and hl.fg
-  return fg and { fg = string.format("#%06x", fg) }
+-- Create floating terminal
+local terminals = {}
+function M.float_term(opts)
+  -- Set options
+  opts = opts or {}
+  local cwd = opts.cwd or vim.uv.cwd()
+  local name = opts.name or short_path_name(cwd)
+  local width = opts.width or math.floor(vim.o.columns * 0.8)
+  local height = opts.height or math.floor(vim.o.lines * 0.8)
+  local row = opts.row or math.floor((vim.o.lines - height) / 2)
+  local col = opts.col or math.floor((vim.o.columns - width) / 2)
+  -- Close existing floating window if exists
+  if terminals[name]
+      and terminals[name].win
+      and vim.api.nvim_win_is_valid(terminals[name].win)
+  then
+    vim.api.nvim_win_close(terminals[name].win, true)
+    terminals[name].win = nil
+    return
+  end
+  -- Set/Store terminal variables
+  local term = terminals[name]
+  local win
+  local buf
+  -- Create/open terminal buffer only once
+  if not term or not vim.api.nvim_buf_is_valid(term.buf) then
+    buf = vim.api.nvim_create_buf(false, true)
+    terminals[name] = {
+      buf = buf,
+      cwd = cwd,
+    }
+    win = vim.api.nvim_open_win(buf, true, {
+      relative = "editor",
+      width = width,
+      height = height,
+      row = row,
+      col = col,
+      style = "minimal",
+    })
+    terminals[name].win = win
+    vim.fn.jobstart(vim.o.shell, { term = true, cwd = cwd })
+    vim.cmd("startinsert!")
+  else
+    -- Reopen existing buf-terminal
+    win = vim.api.nvim_open_win(term.buf, true, {
+      relative = "editor",
+      width = width,
+      height = height,
+      row = row,
+      col = col,
+      style = "minimal",
+    })
+    terminals[name].win = win
+    vim.cmd("startinsert!")
+  end
+vim.print(terminals)
 end
 
 -- Returns the root directory based on:
@@ -32,11 +83,9 @@ end
 -- * lsp root_dir
 -- * root pattern of filename of the current buffer
 -- * root pattern of cwd
-M.root_patterns = { ".git", "lua" }
-
+local root_patterns = { ".git", "lua" }
 function M.get_root()
   local path = vim.api.nvim_buf_get_name(0)
-  path = path ~= "" and vim.uv.fs_realpath(path) or nil
   local roots = {}
   if path then
     for _, client in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
@@ -58,109 +107,10 @@ function M.get_root()
   local root = roots[1]
   if not root then
     path = path and vim.fs.dirname(path) or vim.uv.cwd()
-    root = vim.fs.find(M.root_patterns, { path = path, upward = true })[1]
+    root = vim.fs.find(root_patterns, { path = path, upward = true })[1]
     root = root and vim.fs.dirname(root) or vim.uv.cwd()
   end
   return root
-end
-
--- Opens a floating terminal (interactive by default)
-local terminals = {}
-function M.float_term(cmd, opts)
-  opts = vim.tbl_deep_extend("force", {
-    ft = "floaterm",
-    size = { width = 0.8, height = 0.9 },
-  }, opts or {}, { persistent = true })
-
-  local termkey = vim.inspect({ cmd = cmd or "shell", cwd = opts.cwd, env = opts.env, count = vim.v.count1 })
-
-  if terminals[termkey] and terminals[termkey]:buf_valid() then
-    terminals[termkey]:toggle()
-  else
-    terminals[termkey] = require("lazy.util").float_term(cmd, opts)
-    local buf = terminals[termkey].buf
-    vim.b[buf].lazyterm_cmd = cmd
-    if opts.esc_esc == false then
-      vim.keymap.set("t", "<Esc>", "<Esc>", { buffer = buf, nowait = true })
-    end
-    if opts.ctrl_hjkl == false then
-      vim.keymap.set("t", "<C-h>", "<C-h>", { buffer = buf, nowait = true })
-      vim.keymap.set("t", "<C-j>", "<C-j>", { buffer = buf, nowait = true })
-      vim.keymap.set("t", "<C-k>", "<C-k>", { buffer = buf, nowait = true })
-      vim.keymap.set("t", "<C-l>", "<C-l>", { buffer = buf, nowait = true })
-    end
-
-    vim.api.nvim_create_autocmd("BufEnter", {
-      buffer = buf,
-      callback = function()
-        vim.cmd.startinsert()
-      end,
-    })
-  end
-
-  return terminals[termkey]
-end
-
--- Toggle UI components
-function M.toggle(option, silent, values)
-  local Util = require("lazy.core.util")
-  if values then
-    if vim.opt_local[option]:get() == values[1] then
-      vim.opt_local[option] = values[2]
-    else
-      vim.opt_local[option] = values[1]
-    end
-    return Util.info("Set " .. option .. " to " .. vim.opt_local[option]:get(), { title = "Option" })
-  end
-  vim.opt_local[option] = not vim.opt_local[option]:get()
-  if not silent then
-    if vim.opt_local[option]:get() then
-      Util.warn("Enabled " .. option, { title = "Option" })
-    else
-      Util.warn("Disabled " .. option, { title = "Option" })
-    end
-  end
-end
-
--- Toggle language for spelling en|es
-function M.language()
-  local Util = require("lazy.core.util")
-  if vim.opt_local["spelllang"]:get()[1] == "en" then
-    vim.opt_local["spelllang"] = "es"
-    Util.warn("Enabled spanish language", { title = "Language" })
-  elseif vim.opt_local["spelllang"]:get()[1] == "es" then
-    vim.opt_local["spelllang"] = "en"
-    Util.warn("Enabled english language", { title = "Language" })
-  end
-end
-
--- Toggle line numbers
-local nu = { number = true, relativenumber = true }
-function M.toggle_number()
-  local Util = require("lazy.core.util")
-  if vim.opt_local.number:get() or vim.opt_local.relativenumber:get() then
-    nu = { number = vim.opt_local.number:get(), relativenumber = vim.opt_local.relativenumber:get() }
-    vim.opt_local.number = false
-    vim.opt_local.relativenumber = false
-    Util.warn("Disabled line numbers", { title = "Option" })
-  else
-    vim.opt_local.number = nu.number
-    vim.opt_local.relativenumber = nu.relativenumber
-    Util.warn("Enabled line numbers", { title = "Option" })
-  end
-end
-
--- Toggle diagnostics
-function M.toggle_diagnostics()
-  local Util = require("lazy.core.util")
-  local enabled = vim.diagnostic.is_enabled()
-  if not enabled then
-    vim.diagnostic.enable()
-    Util.info("Enabled diagnostics", { title = "Diagnostics" })
-  else
-    vim.diagnostic.enable(false)
-    Util.warn("Disabled diagnostics", { title = "Diagnostics" })
-  end
 end
 
 return M
