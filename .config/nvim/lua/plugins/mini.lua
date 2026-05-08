06482aede32646bf995aa46ce5539b6596428ae7
Diego Castro G. <castrogda@gmail.com>
2026-05-08 12:57:09 -0500

    neovim: Desactivar resaltado de HEX COLORS para priorizar vim.lsp.document_color

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -10,14 +10,14 @@ return {
     "echasnovski/mini.hipatterns",
     version = "*",
     opts = function()
-      local hipatterns = require("mini.hipatterns")
+      -- local hipatterns = require("mini.hipatterns")
       return {
         highlighters = {
           fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
           hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
           todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
           note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
-          -- hex_color = hipatterns.gen_highlighter.hex_color(),
+          -- hex_color = hipatterns.gen_highlighter.hex_color()
         }
       }
     end,
